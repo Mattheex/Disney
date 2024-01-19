026950529671e56d30f29ad13026ffa629e154f2
Audoux Nicolas <nicolas.audoux@etu.unice.fr>
2024-01-19 20:44:38 +0100

    update micro services

--- micro-service/preprocess.php
+++ micro-service/preprocess.php
@@ -1,39 +1,112 @@
 <?php
-$url="https://superheroapi.com/api/2521867701306092/search/".$_GET['name'] ;
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+#$url="https://superheroapi.com/api/2521867701306092/search/".$_GET['name'] ;
+$baseURL = "https://superheroapi.com/api/2521867701306092/search/";
+$searchTerm = rawurlencode($_GET['name']);
+$url = $baseURL . $searchTerm;
 
-
-function process($text){
-    $start=strpos($text,"(");
-    $end=strpos($text,")")+1;
-    if($start){
-        $rem = substr($text,$start,$end-$start);
-        $res = str_replace($rem,"",$text);
-        return process($res);
+function getEnd($text){
+    $end = strpos($text,")");
+    if($end!==false){
+        return $end+1;
     }
-    $array = explode(",",$text);
+    return strlen($text);
+
+}
+
+function textProcess($text,$group){
+    $text = str_replace(";",",",$text);
+    $text = str_replace("/",",",$text);
+    $text = strtolower($text);  
+    $text = str_replace(" former "," ",$text);
+    $text = str_replace(" formerly "," ",$text);
+    $text = process($text,$group);
+    return $text;
+}
+
+function clearText($word){
+    $word = trim($word);
+    if (substr($word, 0, 2) === 'a ') {
+        $word = substr($word, 2);
+    }   
+    if (substr($word, 0, 7) === 'former ') {
+        $word = substr($word, 7);
+    }  
+
+    return keepAlphanumeric($word);
+
+}
+
+function keepAlphanumeric($str) {
+    $str = strtolower($str);
+    return preg_replace("/[^a-zA-Z0-9]/", "", $str);
+}
+
+
+
+function findId($name,$superName){
+    $name = keepAlphanumeric($name);
+    $superName = keepAlphanumeric($superName);
+    return $name . $superName;
+}
+
+
+
+
+function process($text,$group) {
+    $start = strpos($text, "(");
+    if ($start !== false) {
+        $end = getEnd($text);
+        $rem = substr($text, $start, $end - $start);
+        $res = str_replace($rem, "", $text);
+        return process($res,true);
+    }
+
+    $array = explode(",", $text);
+    if($group){
+        $array = array_map('clearText',$array);
+    }
     return $array;
 }
 
 
-// Make the API call
-$response = file_get_contents($url, false, $context);
 
+
+
+
+// Make the API call
+$response = file_get_contents($url, false);
 
 $decoded = json_decode($response);
 
 $answer=$decoded->{"results"};
 foreach($answer as $perso){
+    $superName = $perso->{"name"};
+    $name = $perso->{'biography'}->{'full-name'};
+    $publisher = $perso->{'biography'}->{'publisher'};
     $connections = $perso->{"connections"};
+    $apparition = $perso->{'biography'}->{'first-appearance'};
+    $groups = $connections->{"group-affiliation"};
     $relatives =  $connections->{"relatives"};
+    $appearance = $perso->{"appearance"};
+    $race = $appearance->{"race"};
     $work = $perso->{"work"};
     $occupation = $work->{"occupation"};
-    $groups = $connections->{"group-affiliation"};
-    $connections->{"relatives"} =  process($relatives);
-    $connections->{"group-affiliation"} =  process($groups);
-    $work->{"occupation"} =  process($occupation);
+    $color_eye = $appearance->{"eye-color"};
+    $color_hair = $appearance->{"hair-color"};
+    $connections->{"relatives"} =  textProcess($relatives,true);
+    $work->{"occupation"} =  textProcess($occupation,true);
+    $appearance->{"hair-color"} = textProcess($color_hair,true);
+    $appearance->{"eye-color"} = textProcess($color_eye,true);
+    $perso->{"id"} = findId($name,$superName);
+    $connections->{"group-affiliation"} = textProcess($groups,False);
+    $appearance->{"race"} = textProcess($race,true);
+    $perso->{'biography'}->{'publisher'} = textProcess($publisher,true);
+    $perso->{'biography'}->{'first-appearance'} = textProcess($apparition,false);
+
+
 }
 
 $ans = json_encode($answer);
 echo $ans;
-
-
